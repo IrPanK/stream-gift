307fff77ba576510a6cbd29c2d910c2540f43006
irfannkamill <irfannkamill@gmail.com>
2022-06-29 23:34:29 +0700

    is it over?

--- index.js
+++ index.js
@@ -39,9 +39,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(process.env.PORT, () =>
-    console.log(`Server running on port: ${PORT}`)
-);
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
 mongoose
     .connect(process.env.CONNECTION_URL)
