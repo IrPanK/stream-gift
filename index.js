b31a127cca9b93d41835d5afccb673eaff12538f
irfannkamill <irfannkamill@gmail.com>
2022-06-29 23:25:17 +0700

    hope this work

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ app.use("/user", userRoutes);
 // greeting
 app.get("/", (req, res) => {
     res.send("Hello to stream gift API");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+    );
 });
 
 const PORT = process.env.PORT || 5000;
